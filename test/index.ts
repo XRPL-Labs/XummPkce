b8eb9f6517578be331dc9f58bb097dab5540d0ae
Wietse Wind <mail@wietse.com>
2022-06-08 21:22:26 +0200

    Fix useless test

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,10 @@
 import { XummPkce } from "../src/index";
 
 describe(`Tests: TODO`, () => {
-  const auth = new XummPkce();
+  const auth = new XummPkce(
+    "d48e0d5c-0800-4f3e-b475-cee59ba56e7a",
+    "http://localhost"
+  );
 
   describe("TODO", () => {
     it(`'TODO`, async () => {
